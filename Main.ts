db47d5eb27a0bd85140d1e85f5d9331a24a60574
Adnan <adnanrajpoot433@gmail.com>
2026-05-18 20:38:17 +0500

    In

---
+++ Main.ts
@@ -0,0 +1,8 @@
+import {appname} from "./Module"
+import {add} from "./Module"
+import {formatter} from "./Module"
+
+
+console.log(appName);
+add(10,20);
+console.log(Formatter.toUpper("welcome"));
---
+++ Module.ts
@@ -0,0 +1,17 @@
+export let appname="calculator";
+
+export function add(a:number,b:number)
+{
+    return a+b;
+}
+
+export class Formatter
+{
+
+    static toUpper(str:string):string
+    {
+        return str.toUpperCase();
+    }
+}
+
+
