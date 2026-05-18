fa0c0e2bd9c36503f6a85d5f160c72f154a95872
Adnan <adnanrajpoot433@gmail.com>
2026-05-18 20:40:38 +0500

    Module Concept

--- Main.ts
+++ Main.ts
@@ -1,8 +1,8 @@
 import {appname} from "./Module"
 import {add} from "./Module"
-import {formatter} from "./Module"
+import {Formatter} from "./Module"
 
 
-console.log(appName);
+console.log(appname);
 add(10,20);
 console.log(Formatter.toUpper("welcome"));
